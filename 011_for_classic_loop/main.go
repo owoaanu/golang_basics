ffb9c41e726fcf359a44fb67628d0151bffd144c
Samuel Owoaanu <sam.adebayoadesina@gmail.com>
2026-01-24 11:39:49 +0100

    I learnt classic for loop in go

---
+++ 011_for_classic_loop/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Iteration:", i)
+	}
+	
+	N := 10
+	sum := 0
+	
+	for i:= 1; i<=N; i++ {
+		sum += i
+	}
+	
+	fmt.Println("Sum of first", N, "natural numbers is:", sum)
+}
